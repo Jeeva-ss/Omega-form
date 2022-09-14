b762767ee5dd25a55f57c25ca41a0528b0364b5d
jeevaf22 <48169919+Jeeva-ss@users.noreply.github.com>
2022-09-14 19:10:22 +0530

    files modified

---
+++ app.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("form");
+const emailValue = document.getElementById("email");
+const password = document.getElementById("password");
+const messageContainer = document.getElementById("message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+
+validateForm = () => {
+	// Check validity
+	isValid = form.checkValidity();
+	!isValid
+		? (message.textContent = "Please fill out all fields")
+		: (message.textContent = "Successfully signed in!");
+};
+
+// Store in console
+storeData = () => {
+	const details = {
+		email: form.email.value,
+		password: form.password.value,
+	};
+	console.log(details);
+};
+
+// Process form
+processForm = (e) => {
+	e.preventDefault();
+	//   Validate form
+	validateForm();
+	isValid ? storeData() : "";
+	form.email.value = "";
+	form.password.value = "";
+};
+
+// Event listener
+form.addEventListener("submit", processForm);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// arr.map(function (x) {
+// 	if (x % 2 === 0) {
+// 		console.log(x);
+// 	}
+// });
